09980dd4a579728e8251a5c08b61a55be0a64ec9
Stanislav Obrazcoff <obrazcoff@gmail.com>
2020-01-22 01:44:54 +0100

    Add input for todo

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Navbar } from './components/Navbar'
+import { AddTodo } from './components/AddTodo'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Navbar title="Todo App" />
+      <AddTodo />
     </View>
   )
 }
---
+++ components/AddTodo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, StyleSheet, TextInput, Button } from 'react-native'
+
+export const AddTodo = (props) => {
+  return (
+    <View style={styles.wrapper}>
+      <TextInput style={styles.input} />
+      <Button title="Add" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderColor: '#3848ab',
+  },
+})
